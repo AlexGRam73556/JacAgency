5aa5f68ef8e3265716f97937a7d449e5a0043adc
Emilio Alejandro Ramírez Gaytán <erg73556@udelasalle.edu.mx>
2023-05-25 16:08:19 -0600

    Initial Commit

---
+++ JacAgency/JacAgencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JacAgencyApp.swift
+//  JacAgency
+//
+//  Created by Emilio Alejandro Ramirez Gaytan on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct JacAgencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
